f289d054a6dcd0d2d80280f64b451ba3e0761f06
Kerim Öztürk <kerim_648@hotmail.com>
2023-02-15 21:38:37 +0300

    user module and login controller have been created

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 
 router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
+router.route('/login').post(authController.loginUser); 
 
 
 
