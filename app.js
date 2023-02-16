0536e8e4772f50f5774539b8414c0f1bf8e0ff31
Kerim Öztürk <kerim_648@hotmail.com>
2023-02-16 12:47:17 +0300

    logout controller added and session has been stored in mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
@@ -21,7 +22,6 @@ mongoose
 // Template Engine
 app.set('view engine', 'ejs');
 
-
 // Global Variable
 
 global.userIN = null;
@@ -35,14 +35,15 @@ app.use(
     secret: 'my_keyboard_cat',
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
   })
 );
 
 // Routes
-app.use('*',(req,res,next) => {
+app.use('*', (req, res, next) => {
   userIN = req.session.userID;
   next();
-})
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
